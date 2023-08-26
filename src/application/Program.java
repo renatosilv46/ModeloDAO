2673849c1418ba5233781ce21edff17a3ae92ea4
Renato <rnt.graca01@gmail.com>
2023-08-26 13:29:21 -0300

    Melhoria das Classes DAO e programa principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,18 +10,12 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String args[]) {
-		
-		Department obj = new Department(1,"Books");
-		
-		System.out.println(obj);
-		
-		Seller seller2 = new Seller(21,"Bob","bob@gmail.com", new Date(),3000.0,obj);
-		
-		
-		
+	
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 	
+		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 	}
 	
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -20,53 +20,63 @@ public class SellerDaoJDBC implements SellerDao{
 		this.conn = conn;
 	}
 	
+	
+	//Método para inserir Seller pelo ID;
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	
+	//Método para atualizar Seller pelo ID;
 	@Override
 	public void update(Seller obj) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	//Método para deletar Seller pelo ID;
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	//Método para buscar Seller pelo ID;
 	@Override
 	public Seller findById(Integer id) {
 		
+	//Inicializando as variáveis de acesso e resultado do banco;
 	PreparedStatement st = null;
 	ResultSet rs = null;
+	
+	//Tratamento de excessões;
 	try {
+		//Variável de acesso ao dado recebendo o valor da query que será executada no BD;
 		st = conn.prepareStatement("SELECT seller.*,department.Name as DepName "
 				+ "FROM seller INNER JOIN department on seller.DepartmentId = department.Id "
 				+ "WHERE seller.Id = ?");
 		
+		//Aqui estou passando o paramêtro que será utilizado na query do BD e executando a query com a var rs;
 		st.setInt(1, id);
 		rs = st.executeQuery();
+		
+		//If usado para colocar a rs na próxima posição caso não tenha nada na posição atual;
+		//Se as próximas posições também não estiverem nenhum valor, é usado o return nulo que está mais abaixo;
 		if(rs.next()) {
-			Department dep = new Department();
-			dep.setId(rs.getInt("DepartmentId"));
-			dep.setName(rs.getString("DepName"));
+		
+			//Estou usando um método onde os valores capturados no rs, já estão sendo setados nos seus devidos atributos; 
+			Department dep = instanciateDepartment(rs);
 			
-			Seller seller = new Seller();
-			seller.setId(rs.getInt("Id"));
-			seller.setName(rs.getString("Name"));
-			seller.setEmail(rs.getString("Email"));
-			seller.setBaseSalary(rs.getDouble("BaseSalary"));
-			seller.setBirthDate(rs.getDate("BirthDate"));
-			seller.setDepartment(dep);
-		return seller;
+			Seller seller = instanciateSeller(rs, dep);
+						return seller;
+
 		}
-	return null;
+	//Usado para retornar nulo caso não tenha nenhum valor no resultSet;
+		return null;
 	
-	
+	//Tratamento de excessões;
 	}catch (SQLException e) {
 		throw new DbException(e.getMessage());
 		
@@ -78,6 +88,27 @@ public class SellerDaoJDBC implements SellerDao{
 		
 	}
 
+	//Método usado para setar os valores do rs nos seus devidos atributos de cada entidade;
+	private Seller instanciateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller seller = new Seller();
+			seller.setId(rs.getInt("Id"));
+			seller.setName(rs.getString("Name"));
+			seller.setEmail(rs.getString("Email"));
+			seller.setBaseSalary(rs.getDouble("BaseSalary"));
+			seller.setBirthDate(rs.getDate("BirthDate"));
+			seller.setDepartment(dep);
+		return seller;
+	}
+
+	private Department instanciateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		
+		return dep;
+	}
+
+	//Método para buscar uma lista Seller;
 	@Override
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
