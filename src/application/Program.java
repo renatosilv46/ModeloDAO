55fd8c4a1ee1948278aaa59d991a965814828622
Renato <rnt.graca01@gmail.com>
2023-08-27 15:15:17 -0300

    Criando método para listar sellers usando o mesmo objeto Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -14,9 +14,15 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 	
 		System.out.println("=== TEST 1: seller findById ===");
-		Seller seller = sellerDao.findById(3);
-		
-		System.out.println(seller);
+		Seller obj = sellerDao.findById(3);
+		System.out.println(obj);
+		System.out.println("\n=== TEST 2: seller findByDepartment ===");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for(Seller seller : list) {
+			System.out.println(seller);
+		}
+			
 	}
 	
 }
